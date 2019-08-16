306e4051b7609fd1238dd997825c7aa341c71ddf
leocavalcante <lc@leocavalcante.com>
2019-08-16 19:04:47 -0300

    Move COMPOSER_ALLOW_SUPERUSER env to the base image

--- base/install_composer.sh
+++ base/install_composer.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-COMPOSER_ALLOW_SUPERUSER=1
 EXPECTED_SIGNATURE="$(curl https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
 then
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
+  >&2 echo 'ERROR: Invalid installer signature'
+  rm composer-setup.php
+  exit 1
 fi
 
 php composer-setup.php --quiet
