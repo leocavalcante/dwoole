17fd61f2a6244d6dd19c7db2e9d291eaeac5eae5
leocavalcante <lc@leocavalcante.com>
2019-08-16 19:03:25 -0300

    Testing changes

--- development/example/index.php
+++ development/example/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Siler\Swoole;
 
 $server = function () {
-    Swoole\emit('Hello World');
+    Swoole\emit('Hello World!');
 };
 
 Swoole\http($server)->start();
